80c61c05f01e447ca3716a0d87da7f88db3f4307
Your Name <you@example.com>
2021-12-09 22:09:03 +0900

    Route

--- index.js
+++ index.js
@@ -4,13 +4,16 @@ const express = require("express");
 const app = express();
 const port = 5000;
 const bodyParser = require("body-parser");
-const { User } = require("./models/User");
+const cookieParser = require("cookie-parser");
 const config = require("./config/key");
+const { User } = require("./models/User");
+const { auth } = require("./middleware/auth");
 
 //application/x-www-form-urlencoded 위 데이터를 분석해 가져올 수 있게 하는 것
 app.use(bodyParser.urlencoded({ extended: true }));
 //application/json 타입을 분석해서 가져올 수 있게하는 코드
 app.use(bodyParser.json());
+app.use(cookieParser());
 
 const mongoose = require("mongoose");
 mongoose
@@ -22,14 +25,74 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.post("/register", (req, res) => {
+//회원 가입 route
+app.post("/api/users/register", (req, res) => {
   //회원 가입 할때 필요한 정보를 client에서 가져오면
   //그 데이터를 DB에 넣어준다.
   const user = new User(req.body);
-
   user.save((err, userInfo) => {
-    if (err) return res.json({ success: false });
+    if (err) return res.json({ success: false, err });
     return res.status(200).json({
+      success: true,
+    });
+  });
+});
+
+//login route
+app.post("/login", (req, res) => {
+  //요청된 이메일을 DB에서 있는지 찾아야 한다.
+  User.findOne({ email: req.body.email }, (err, user) => {
+    if (!user) {
+      return res.json({
+        loginSuccess: false,
+        message: "제공된 이메일에 해당하는 유저가 없습니다.",
+      });
+    }
+
+    //DB에 있다면 비밀번호도 같은지 확인해야한다.
+
+    user.comparePassword(req.body.password, (err, isMatch) => {
+      if (!isMatch)
+        return res.json({
+          loginSuccess: false,
+          message: "비밀번호가 틀렸습니다.",
+        });
+      //비밀번호 까지 동일하다면 Token을 생성해야한다.
+      //token route
+      user.generateToken((err, user) => {
+        if (err) return res.status(400).send(err);
+
+        // 토큰을 저장한다. 어디에 ? 쿠키, 로컬스토리지 등에 할 수 있다.
+        res
+          .cookie("x_auth", user.token)
+          .status(200)
+          .json({ loginSuccess: true, userId: user._id });
+      });
+    });
+  });
+});
+
+//auth route
+//role은 본인이 개정이 가능함 현재는 role 0 == 일반유저 0이 아니면 관리자
+app.get("/api/users/auth", auth, (req, res) => {
+  //여기 까지 미들웨어를 통과해 왔다는 얘기는 Authentication이 True라는 말.
+  res.status(200).json({
+    _id: req.user._id,
+    isAdmin: req.user.role === 0 ? false : true,
+    isAuth: true,
+    email: req.user.email,
+    name: req.user.name,
+    lastname: req.user.lastname,
+    role: req.user.role,
+    image: req.user.image,
+  });
+});
+
+//logout Route
+app.get("/api/users/logout", auth, (req, res) => {
+  User.findOneAndUpdate({ _id: req.user._id }, { token: "" }, (err, user) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).send({
       success: true,
     });
   });

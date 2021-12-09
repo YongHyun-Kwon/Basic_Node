53f7b5ab0720cc448b4ce983c707eb23304302cd
Your Name <you@example.com>
2021-12-09 17:05:11 +0900

    비밀 정보 보호

--- index.js
+++ index.js
@@ -3,18 +3,36 @@
 const express = require("express");
 const app = express();
 const port = 5000;
+const bodyParser = require("body-parser");
+const { User } = require("./models/User");
+const config = require("./config/key");
+
+//application/x-www-form-urlencoded 위 데이터를 분석해 가져올 수 있게 하는 것
+app.use(bodyParser.urlencoded({ extended: true }));
+//application/json 타입을 분석해서 가져올 수 있게하는 코드
+app.use(bodyParser.json());
 
 const mongoose = require("mongoose");
 mongoose
-  .connect(
-    "mongodb+srv://Hyun:yhkwon0820%23@boilerplate.kw5ew.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
-    {}
-  )
+  .connect(config.mongoURI, {})
   .then(() => console.log("MongoDB Connected.."))
   .catch((err) => console.log(err));
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+app.post("/register", (req, res) => {
+  //회원 가입 할때 필요한 정보를 client에서 가져오면
+  //그 데이터를 DB에 넣어준다.
+  const user = new User(req.body);
+
+  user.save((err, userInfo) => {
+    if (err) return res.json({ success: false });
+    return res.status(200).json({
+      success: true,
+    });
+  });
 });
 
 app.listen(port, () => {
